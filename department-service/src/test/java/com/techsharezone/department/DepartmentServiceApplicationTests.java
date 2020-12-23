b86511d3dbfe92419f76ade9e59730eccc932e71
Saurabh Sharma <saurabhshcs@gmail.com>
2020-12-23 01:22:23 +0000

    Initial commit for Project Structure.

---
+++ department-service/src/test/java/com/techsharezone/department/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techsharezone.department;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
